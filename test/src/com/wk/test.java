bb7767320d3bd3c7373c558187ba8ea4da086eb9
FirstRespository <643515622@qq.com>
2017-01-19 15:14:08 +0800

    初始导入

---
+++ test/src/com/wk/test.java
@@ -0,0 +1,34 @@
+package com.wk;
+
+public class Test {
+	public static void main(String[] args) {
+		//System.out.print("-------------测试-------");
+		System.out.print("-------------测试03-------");
+		System.out.print("-------------测试04-------");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
