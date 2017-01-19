b1c3fdfcc8e0fc29c265accbf1de72e14f5ba2f6
FirstRespository <643515622@qq.com>
2017-01-19 15:24:11 +0800

    04

--- test/src/com/wk/test.java
+++ test/src/com/wk/test.java
@@ -2,7 +2,8 @@ package com.wk;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.print("-------------测试-------");
+		//System.out.print("-------------测试-------");
+		
 		System.out.print("-------------测试03-------");
 		System.out.print("-------------测试04-------");
 	}
